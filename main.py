a69a8ff80a96d76cbcdb9f3cb9cb42445b979576
Oscar-Trombini <134261962+Oscar-Trombini@users.noreply.github.com>
2023-11-09 14:54:36 +0000

    lezione 1

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print('ciao')
+x = 3
+y ='gatto'
+l = [1,2]
+
+
+diz = {'Ciao': 'prova', 'Bye': 333 }
+
+print('ciao , {}' .format(diz['Ciao']))
+
+
+if(x > 4):
+ print('ciaoo')
+else:
+ print('ciaoooo')
+
+for item in l:
+  print(item)
+
+if(x > 4):
+ print('nain')
+elif(x==3):
+ print('bbbb')
+
+#while
+i=0
+while i<10:
+ print(i)
+ i = i+1
+
+print('taaaaaaaaaaaaaaaaab')
+
+#for
+for j in range(10):
+  print(j)
+  
+#funzioni
+def funz(a,b):
+  print("funziona: {} e {}".format(a,b))
+
+funz("pippo","cocacola")
+
+#funzione con ritorno
+def eleva(n):
+  return n*n
+
+print('funge: {}'.format(eleva(4)))
